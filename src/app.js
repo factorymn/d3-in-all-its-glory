0cf56fdbdd0c60897d6199d656c1fc864d6a47d7
levvsha <levsha.mikhail@gmail.com>
2018-01-22 09:15:28 +0300

    feat: add step two

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import drawFunction from './stepOne';
+import drawFunction from './stepTwo';
 
 drawFunction();
 
-module.hot.accept('./stepOne', () => {
-  const newDrawFunction = require('./stepOne').default;
+module.hot.accept('./stepTwo', () => {
+  const newDrawFunction = require('./stepTwo').default;
 
   document.getElementsByClassName('legend')[0].innerHTML = '';
   document.getElementsByClassName('chart')[0].innerHTML = '';
