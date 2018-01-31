1bd7e880fbf58eb2b4407fc8351f59929ece3721
levvsha <levsha.mikhail@gmail.com>
2018-01-31 09:11:56 +0300

    feat: change interpolation of lines rework styles for axis and ticks

--- src/stepOne.js
+++ src/stepOne.js
@@ -22,7 +22,7 @@ const data = d3.csvParse(dataAsStringRu, d => d);
 export default function draw() {
   const margin = { top: 20, right: 20, bottom: 50, left: 50 };
   const width = 920 - margin.left - margin.right;
-  const height = 360 - margin.top - margin.bottom;
+  const height = 390 - margin.top - margin.bottom;
 
   const x = d3.scaleTime()
     .range([0, width]);
@@ -55,16 +55,17 @@ export default function draw() {
 
   const yAxis = d3.axisRight(y)
     .ticks(5)
-    .tickSize(width)
-    .tickPadding(-20 - width);
+    .tickSize(7 + width)
+    .tickPadding( -15 - width);
 
   svg.append('g')
-    .attr('class', 'axis')
+    .attr('class', 'axis y-axis')
     .attr('transform', `translate(0,${ height })`)
     .call(xAxis);
 
   svg.append('g')
-    .attr('class', 'axis')
+    .attr('transform', 'translate(-7, 0)')
+    .attr('class', 'axis x-axis')
     .call(yAxis);
 
   svg.append('g')
@@ -91,7 +92,8 @@ export default function draw() {
 
   const lineGenerator = d3.line()
     .x(d => x(d.date))
-    .y(d => y(d.percent));
+    .y(d => y(d.percent))
+    .curve(d3.curveCardinal);
 
   svg
     .selectAll('.line')
