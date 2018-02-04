7b8f39f1a6e7338ce60289378a516b9377f21af6
levvsha <levsha.mikhail@gmail.com>
2018-02-04 17:14:30 +0300

    feat: add demo file with step one

---
+++ src/demo.js
@@ -0,0 +1,100 @@
+d3.timeFormatDefaultLocale({
+  'dateTime': '%A, %e %B %Y г. %X',
+  'date': '%d.%m.%Y',
+  'time': '%H:%M:%S',
+  'periods': ['AM', 'PM'],
+  'days': ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+  'shortDays': ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'],
+  'months': ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'],
+  'shortMonths': ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
+});
+
+d3.csv('https://raw.githubusercontent.com/factorymn/d3-in-all-its-glory/master/stats/data.csv', draw);
+
+function draw(data) {
+  const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+  const width = 920 - margin.left - margin.right;
+  const height = 390 - margin.top - margin.bottom;
+
+  const x = d3.scaleTime()
+    .range([0, width]);
+
+  const y = d3.scaleLinear()
+    .range([height, 0]);
+
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory20);
+
+  const svg = d3.select('.chart')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${ margin.left },${ margin.top })`);
+
+  data.forEach(d => {
+    d.date = new Date(d.date);
+    d.percent = +d.percent;
+  });
+
+  x.domain(d3.extent(data, d => d.date));
+  y.domain([0, d3.max(data, d => d.percent)]);
+  colorScale.domain(d3.map(data, d => d.regionId).keys());
+
+  const xAxis = d3.axisBottom(x)
+    .ticks((width + 2) / (height + 2) * 5)
+    .tickSize(-height - 6)
+    .tickPadding(10);
+
+  const yAxis = d3.axisRight(y)
+    .ticks(5)
+    .tickSize(7 + width)
+    .tickPadding(-15 - width)
+    .tickFormat(d => d + '%');
+
+  svg.append('g')
+    .attr('class', 'axis x-axis')
+    .attr('transform', `translate(0,${ height + 6 })`)
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', 'translate(-7, 0)')
+    .attr('class', 'axis y-axis')
+    .call(yAxis);
+
+  svg.append('g')
+    .attr('transform', `translate(0,${ height })`)
+    .call(d3.axisBottom(x).ticks(0));
+
+  svg.append('g')
+    .call(d3.axisLeft(y).ticks(0));
+
+  const nestByRegionId = d3.nest()
+    .key(d => d.regionId)
+    .sortKeys((v1, v2) => (parseInt(v1, 10) > parseInt(v2, 10) ? 1 : -1))
+    .entries(data);
+
+  const regions = {};
+
+  d3.map(data, d => d.regionId)
+    .keys()
+    .forEach((d, i) => {
+      regions[d] = nestByRegionId[i].values;
+    });
+
+  const regionIds = Object.keys(regions);
+
+  const lineGenerator = d3.line()
+    .x(d => x(d.date))
+    .y(d => y(d.percent))
+    .curve(d3.curveCardinal);
+
+  svg
+    .selectAll('.line')
+    .data(regionIds)
+    .enter()
+    .append('path')
+    .attr('class', 'line')
+    .attr('id', regionId => `region-${ regionId }`)
+    .attr('d', regionId => lineGenerator(regions[regionId]))
+    .style('stroke', regionId => colorScale(regionId));
+}
